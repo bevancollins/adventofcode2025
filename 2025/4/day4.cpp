d8993afcce62edfa79f21cc677bfaaa7d74ae607
Bevan Collins <b.collins@sentrybay.com>
2025-12-04 20:07:36 +1300

    day 4 part 2

--- 2025/4/day4.cpp
+++ 2025/4/day4.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <cassert>
 #include <string>
+#include <vector>
 
 bool has_roll(const std::string& line, int index) {
   return index >= 0 && line.length() >= index && line[index] == '@';
 }
 
-int rolls_available(const std::string& prev, const std::string& current, const std::string& next) {
+int rolls_available(std::string& prev, std::string& current, std::string& next, bool remove) {
   int available{};
   for (int i = 0; i < current.length(); i++) {
     if (has_roll(current, i)) {
@@ -30,12 +31,43 @@ int rolls_available(const std::string& prev, const std::string& current, const s
       if (has_roll(prev, i - 1))    // NW
         nearby_rolls++;
 
-      if (nearby_rolls < 4)
+      if (nearby_rolls < 4) {
         available++;
+
+        if (remove)
+          current[i] = '.';
+      }
     }
   }
 
   return available;
+}
+
+int total_available(std::vector<std::string>& lines, bool remove) {
+  std::string blank;
+  auto total = rolls_available(blank, lines[0], lines[1], remove);
+
+  for (int i = 1; i < lines.size() - 1; i++)
+    total += rolls_available(lines[i - 1], lines[i], lines[i + 1], remove);
+
+  total += rolls_available(lines[lines.size() - 2], lines[lines.size() - 1], blank, remove);
+
+  return total;
+}
+
+int total_part1(std::vector<std::string>& lines) {
+  return total_available(lines, false);
+}
+
+int total_part2(std::vector<std::string>& lines) {
+  int prev_total;
+  int total{};
+  do {
+    prev_total = total;
+    total += total_available(lines, true);
+  } while (prev_total != total);
+
+  return total;
 }
 
 int main(int argc, char** argv) {
@@ -55,33 +87,16 @@ int main(int argc, char** argv) {
       out = &fout;
     }
 
-    int total{};
-    std::string prev, current, next;
+    std::vector<std::string> lines;
+    std::string line;
+    while(std::getline(fin, line)) {
+      lines.push_back(line);
+    }
 
-    bool first = true;
-    bool last = false;
-    do {
-      if (first) {
-        first = false;
-        if (!std::getline(fin, current)) break;
-        if (!std::getline(fin, next)) break;
-      } else {
-        prev = current;
-        current = next;
-
-        if (!std::getline(fin, next)) {
-          next = {};
-          last = true;
-        }
-      }
-
-      auto rolls = rolls_available(prev, current, next);
-      *out << rolls << std::endl;
-      total += rolls;
-
-    } while (!last);
-
-    *out << "part1: " << total << std::endl;
+    if (lines.size() >= 3) {
+      *out << "part1: " << total_part1(lines) << std::endl;
+      *out << "part2: " << total_part2(lines) << std::endl;
+    }
 
     return EXIT_SUCCESS;
  } catch (std::exception& e) {

99af3669db0d2fd1d6c9a51c41c9ce69bdf3c88b
Bevan Collins <b.collins@sentrybay.com>
2025-12-08 20:39:33 +1300

    2025 day 7 part 2

--- 2025/7/day7.cpp
+++ 2025/7/day7.cpp
@@ -27,31 +27,37 @@ int main(int argc, char** argv) {
     assert(!line.empty());
 
     auto width = line.length();
-    auto tachyon_manifold = std::make_unique<bool[]>(width);
+    auto quantum_tachyon_manifold = std::make_unique<long long[]>(width);
     auto pos = line.find('S');
     assert(pos != std::string::npos);
-    tachyon_manifold[pos] = true;
+    quantum_tachyon_manifold[pos] = 1;
 
     int split_count{};
 
     while(std::getline(fin, line)) {
-      int splitter_pos{};
+      int splitter_pos{-1};
       for(;;) {
-        splitter_pos = line.find('^', splitter_pos);
+        splitter_pos = line.find('^', splitter_pos + 1);
+        assert(splitter_pos == std::string::npos || splitter_pos < width - 1);
         if (splitter_pos == std::string::npos) {
           break;
-        } else if (tachyon_manifold[splitter_pos]) {
+        } else if (quantum_tachyon_manifold[splitter_pos] != 0) {
           split_count++;
-          tachyon_manifold[splitter_pos] = false;
-          tachyon_manifold[splitter_pos - 1] = true;
-          tachyon_manifold[splitter_pos + 1] = true;
+
+          quantum_tachyon_manifold[splitter_pos - 1] += quantum_tachyon_manifold[splitter_pos];
+          quantum_tachyon_manifold[splitter_pos + 1] += quantum_tachyon_manifold[splitter_pos];
+          quantum_tachyon_manifold[splitter_pos] = 0;
         }
-        splitter_pos++;
       }
     }
 
+    long long timelines{};
+    for (int i = 0; i < width; i++)
+      timelines += quantum_tachyon_manifold[i];
+
     *out
-      <<  split_count << std::endl;
+      << "part 1: " << split_count << std::endl
+      << "part 2: " << timelines << std::endl;
 
     return EXIT_SUCCESS;
  } catch (std::exception& e) {
